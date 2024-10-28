e1a3cd280ad0127ccecb842c701e95416a8d314d
jhoan perez <devnuvi@gmail.com>
2024-10-28 17:32:41 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:connectycube_flutter_call_kit/connectycube_flutter_call_kit.dart';
+import 'package:flutter/material.dart';
+
+import 'package:connectycube_sdk/connectycube_sdk.dart';
+
+import 'src/login_screen.dart';
+import 'src/utils/configs.dart' as config;
+import 'src/utils/pref_util.dart';
+
+void main() => runApp(const App());
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AppState();
+  }
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      home: Builder(
+        builder: (context) {
+          return const LoginScreen();
+        },
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    ConnectycubeFlutterCallKit.instance.init();
+
+    initConnectycube();
+  }
+}
+
+initConnectycube() {
+  init(
+    config.appId,
+    config.authKey,
+    config.authSecret,
+    onSessionRestore: () {
+      return SharedPrefs.getUser().then((savedUser) {
+        return createSession(savedUser);
+      });
+    },
+  );
+  setEndpoints(config.apiEndpoint, config.chatEndpoint);
+}
+
+initConnectycubeContextLess() {
+  CubeSettings.instance.applicationId = config.appId;
+  CubeSettings.instance.authorizationKey = config.authKey;
+  CubeSettings.instance.authorizationSecret = config.authSecret;
+  CubeSettings.instance.onSessionRestore = () {
+    return SharedPrefs.getUser().then((savedUser) {
+      return createSession(savedUser);
+    });
+  };
+
+  setEndpoints(config.apiEndpoint, config.chatEndpoint);
+}
---
+++ lib/src/utils/configs.dart
@@ -0,0 +1,24 @@
+import 'package:connectycube_sdk/connectycube_sdk.dart';
+
+const String appId = "476";
+const String authKey = "f6kU3fThOg7CQFv";
+const String authSecret = "5VVM5XORECzBAgA";
+const String apiEndpoint = 'https://api.connectycube.com';
+const String chatEndpoint = 'chat.connectycube.com';
+
+const String defaultPass = "xxasBUM3gQs36bhj";
+
+List<CubeUser> users = [
+  CubeUser(
+    id: 12806753,
+    login: "call_user_1",
+    fullName: "jhoan perez",
+    password: defaultPass,
+  ),
+  CubeUser(
+    id: 12806757,
+    login: "call_user_2",
+    fullName: "julian riveros",
+    password: defaultPass,
+  ),
+];
